f36aa68b00bd876dd4106b53d44e4537940768a1
Mehedi Hasan Rafi <mehedihasanrafi205@gmail.com>
2026-09-13 16:10:24 +0600

    feat: enhance PatientRegistrationSchema validation for confirmPassword and contactNumber

--- src/validation/auth.validation.ts
+++ src/validation/auth.validation.ts
@@ -15,23 +15,35 @@ export const loginSchema = z.object({
     ),
 });
 
-export const PatientRegistrationSchema = z.object({
-  name: z
-    .string("Name must be a string!!!")
-    .min(3, "Name must be at least 3 characters long!!!")
-    .max(50, "Name must be less than 50 characters long!!!"),
-  email: z.email("Invalid email address!!!"),
-  password: z
-    .string()
-    .min(8, "Password Must Minimum 8 Characters Long.")
-    .regex(/[a-z]/, "Password must contain at least 1 Lowercase Letter")
-    .regex(/[A-Z]/, "Password must contain at least 1 Uppercase Letter")
+export const PatientRegistrationSchema = z
+  .object({
+    name: z
+      .string("Name must be a string!!!")
+      .min(3, "Name must be at least 3 characters long!!!")
+      .max(50, "Name must be less than 50 characters long!!!"),
+    email: z.email("Invalid email address!!!"),
+    password: z
+      .string()
+      .min(8, "Password Must Minimum 8 Characters Long.")
+      .regex(/[a-z]/, "Password must contain at least 1 Lowercase Letter")
+      .regex(/[A-Z]/, "Password must contain at least 1 Uppercase Letter")
 
-    .regex(/[0-9]/, "Password must contain at least 1 Number")
-    .regex(
-      /[^A-Za-z0-9]/,
-      "Password must contain at least 1 Special Character",
-    ),
-  confirmPassword: z.string().min(1, "Please confirm your password"),
-  contactNumber: z.string().optional(),
-});
+      .regex(/[0-9]/, "Password must contain at least 1 Number")
+      .regex(
+        /[^A-Za-z0-9]/,
+        "Password must contain at least 1 Special Character",
+      ),
+    confirmPassword: z
+      .string()
+      .refine((val) => val === "" || /^(?:\+?880|0)1[3-9]\d{8}$/.test(val), {
+        message: "Please provide valid Bangladeshi number",
+      }),
+    contactNumber: z.string().optional(),
+  })
+  .refine((data) => {
+    (data.password === data.confirmPassword,
+      {
+        message: "Password do not match",
+        path: ["confirmPassword"],
+      });
+  });
