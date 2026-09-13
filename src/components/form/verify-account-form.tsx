e516f784a4a76613070347684c7bd65a9a652e7d
Mehedi Hasan Rafi <mehedihasanrafi205@gmail.com>
2026-09-13 21:31:17 +0600

    feat: implement OTP input field and verify account card layout
    - Create VerifyAccountForm card UI with header, content, and footer sections
    - Integrate InputOTP component with 6-digit slot pattern validation
    - Add OTP state management and form submit handler

--- src/components/form/verify-account-form.tsx
+++ src/components/form/verify-account-form.tsx
@@ -1,15 +1,77 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
+
+import { InputOTP, InputOTPGroup, InputOTPSlot } from "../ui/input-otp";
+import { Field, FieldLabel } from "../ui/field";
+import { Button } from "../ui/button";
+import { useState } from "react";
+import { REGEXP_ONLY_DIGITS } from "input-otp";
 
 const VerifyAccountForm = () => {
   const searchParams = useSearchParams();
+  const [otp, srtOtp] = useState("");
   const email = searchParams.get("email");
 
+  const handleOTP = () => {
+    console.log(otp);
+  };
+
   return (
-    <div>
-      <h1>user Email : {email}</h1>
-    </div>
+    <Card>
+      <CardHeader>
+        <CardTitle>Verify Account</CardTitle>
+        <CardDescription>
+          Please provide the OTP we send you in your email
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form
+          id="otp-form"
+          onSubmit={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            handleOTP();
+          }}
+        >
+          <Field>
+            <FieldLabel htmlFor="otp">OTP</FieldLabel>
+            <InputOTP
+              maxLength={6}
+              onChange={(value) => srtOtp(value)}
+              autoComplete="off"
+              name="otp"
+              id="otp"
+              pattern={REGEXP_ONLY_DIGITS}
+            >
+              <InputOTPGroup>
+                <InputOTPSlot index={0} />
+                <InputOTPSlot index={1} />
+                <InputOTPSlot index={2} />
+                <InputOTPSlot index={3} />
+                <InputOTPSlot index={4} />
+                <InputOTPSlot index={5} />
+              </InputOTPGroup>
+            </InputOTP>
+          </Field>
+        </form>
+      </CardContent>
+
+      <CardFooter>
+        <Button>Resend</Button>
+        <Button type="submit" form="otp-form">
+          Submit
+        </Button>
+      </CardFooter>
+    </Card>
   );
 };
 
